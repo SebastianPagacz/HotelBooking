7647c05afd1c58ef940110c9aac75588bdc90d34
SebastianPagacz <234595@student.uek.krakow.pl>
2025-05-16 12:07:13 +0200

    Controller tests + product exceptions

---
+++ HotelBooking.Domain/Exceptions/ProductExceptions/ProductAlreadyExistsException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Domain.Exceptions.ProductExceptions;
+
+public class ProductAlreadyExistsException : Exception
+{
+    public ProductAlreadyExistsException() : base("Product already exisits") { }
+    public ProductAlreadyExistsException(Exception innerException) : base("Product already exisits", innerException) { }
+}
---
+++ HotelBooking.Tests/IntegrationTests/ProductControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace HotelBooking.Tests.IntegrationTests;
+
+public class ProductControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+    public ProductControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Get_CheckBaseParams_ReturnsSuccessStatusCode()
+    {
+        // arrange & act
+        var response = await _client.GetAsync("/api/product");
+        
+        // assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetById_CheckBaseParams_ReturnsSuccessStatusCode()
+    {
+        // arrange & act
+        var response = await _client.GetAsync("/api/product/1");
+
+        // assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
