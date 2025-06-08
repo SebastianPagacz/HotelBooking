eafb1e0c0e9f0d5356da47f828680e137b9be354
SebastianPagacz <234595@student.uek.krakow.pl>
2025-06-08 23:20:43 +0200

    BookingService started

---
+++ Booking.Domain/Models/Booking.cs
@@ -0,0 +1,16 @@
+﻿using Booking.Domain.Enums;
+
+namespace Booking.Domain.Models;
+
+public class Booking
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int ClientId { get; set; }
+    public int PaymentId { get; set; }
+    public DateOnly StartDate { get; set; }
+    public DateOnly EndDate { get; set; }
+    public decimal CaluclatedPrice { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public BookingStatusEnum Status { get; set; }
+}
